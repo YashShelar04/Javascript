4158140b5b37683095e136b2ddc908f45e0f53c7
Yash Shelar <118765043+YashShelar04@users.noreply.github.com>
2023-08-24 11:28:05 +0530

    Add files via upload

---
+++ Practice.js
@@ -0,0 +1,27 @@
+var flag=1
+while(flag ==1){
+let age=prompt("Enter your age")
+age=Number.parseInt(age)
+if(age>=18){
+    alert("You are eligible to drive")
+}else if((age<18)&&(age>0)){
+    alert("You are not eligible to drive")
+}else if(age<0){
+    console.error("Enter the valid age")
+    break;
+}else{
+    break;
+}
+if(age>4){
+    location.href="https://google.com/"
+}
+let recheck=confirm("Do you want to see the prompt again")
+if(recheck){
+    flag =1
+}else{
+    flag =0
+}
+}
+
+var color=prompt("Enter the colour of background")
+document.body.style.background= color;
